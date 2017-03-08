3ef175efeec37dcfec8bf84faeb57129fcedb936
Radek Koubsky <rkoubsky@redhat.com>
2017-03-08 15:26:38 +0100

    Arquillian cube integration test.

--- src/test/java/org/obsidiantoaster/quickstart/OpenShiftIT.java
+++ src/test/java/org/obsidiantoaster/quickstart/OpenShiftIT.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2016-2017 Red Hat, Inc, and individual contributors.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,25 +15,41 @@
  */
 package org.obsidiantoaster.quickstart;
 
+import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.client.KubernetesClient;
-import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.assertj.core.api.Assertions;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static io.fabric8.kubernetes.assertions.Assertions.assertThat;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RunWith(Arquillian.class)
-@RunAsClient
 public class OpenShiftIT {
 
-    @ArquillianResource
-    KubernetesClient client;
+	@ArquillianResource
+	KubernetesClient client;
 
-    @Test
-    public void testAppProvisionsRunningPods() throws Exception {
-        assertThat(client).deployments().pods().isPodReadyForPeriod();
-    }
+	@Test
+	public void testRunningPod() throws Exception {
+		Assertions.assertThat(findPods().iterator()
+				.next()
+				.getStatus()
+				.getPhase())
+				.isEqualToIgnoringCase("running");
+	}
+
+	private List<Pod> findPods() {
+		return this.client.pods()
+				.list()
+				.getItems()
+				.stream()
+				.filter(pod -> !pod.getMetadata()
+						.getName()
+						.contains("build"))
+				.collect(Collectors.toList());
+	}
 
 }
